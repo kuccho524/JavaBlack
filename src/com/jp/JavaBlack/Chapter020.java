d4d7be3ecbf2f1eea06f47eec2a92687bcf8ffb7
kuccho524 <kucchachachanocha@gmail.com>
2021-08-07 11:37:11 +0900

    chapter2-20

---
+++ src/com/jp/JavaBlack/Chapter020.java
@@ -0,0 +1,8 @@
+package com.jp.JavaBlack;
+
+public class Chapter020 {
+	public static void main(String[] args) {
+		StringBuilder sb = new StringBuilder("abcde");
+		System.out.println(sb.capacity());
+	}
+}
